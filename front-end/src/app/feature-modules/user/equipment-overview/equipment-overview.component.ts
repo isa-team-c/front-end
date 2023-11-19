510682b8342b92ac504d863f2e9f02aef1b3045f
adrian-antonic <116621818+adrian-antonic@users.noreply.github.com>
2023-11-19 21:56:28 +0100

    equipment search and filter done

--- front-end/src/app/feature-modules/user/equipment-overview/equipment-overview.component.ts
+++ front-end/src/app/feature-modules/user/equipment-overview/equipment-overview.component.ts
@@ -9,6 +9,9 @@ import { UserService } from '../user.service';
 })
 export class EquipmentOverviewComponent {
   equipment: Equipment[] = [];
+  filteredEquipment: Equipment[] = [];
+  searchQuery: string = '';
+  selectedType: string = '';
 
   constructor(private userService: UserService) {}
 
@@ -20,11 +23,36 @@ export class EquipmentOverviewComponent {
     this.userService.getAllEquipment().subscribe(
       (data) => {
         this.equipment = data;
+        this.filteredEquipment = [...this.equipment];
       },
       (error) => {
         console.error('Error loading equipment:', error);
       }
     );
   }
-  
+
+  filterEquipment() {
+    let filteredByName = this.equipment.filter(equipment =>
+      equipment.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+
+    if (this.selectedType) {
+      this.filteredEquipment = filteredByName.filter(equipment =>
+        equipment.type.toLowerCase() === this.selectedType.toLowerCase()
+      );
+    } else {
+      this.filteredEquipment = filteredByName;
+    }
+  }
+
+  resetFilters() {
+    this.searchQuery = '';
+    this.selectedType = '';
+    this.filterEquipment();
+  }
+
+  getUniqueTypes(): string[] {
+    const types = this.equipment.map(equipment => equipment.type);
+    return [...new Set(types)];
+  }
 }
